c5e3dfecb4b2e240e7bf4fa1618caa133e6170aa
Jenny Wong <yintingjenny.wong@gmail.com>
2020-11-24 12:51:29 +0100

    updated Menu

--- src/Menu.java
+++ src/Menu.java
@@ -4,15 +4,74 @@ public class Menu {
     public static void main(String[] args) {
         printHeader();
         printMenu();
-        getUserInput();
+
+        int userInput;
+
+        do {
+            userInput = getUserInput(6,"your service");
+            switch (userInput) {
+                case 1:
+                    displayAllEvents();
+                    break;
+                case 2:
+                    display5TopEvents();
+                    break;
+                case 3:
+                    displayAllEventsWithOpeningSpots();
+                    break;
+                case 4:
+                    displayAllUpComingEvents();
+                    break;
+                case 5:
+                    displayAllPastEvents();
+                    break;
+                case 6:
+                    enterNewEvents();
+                    break;
+            }
+        } while (userInput != 0);
     }
 
-    private static int getUserInput() {
+    private static void enterNewEvents() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which event you would like to watch?");
+        String name = scanner.nextLine();
+        System.out.println("How much you would like to put in the price pool?");
+        String pricePool = scanner.nextLine();
+        System.out.println("Event Type");
+        for (int i = 0; i < EventCategories.values().length; i++) {
+            EventCategories currentCategory = EventCategories.values()[i];
+            System.out.println((i + 1) + ": " + currentCategory);
+        }
+
+        int eventCategoryChoice = getUserInput(EventCategories.values().length, "an event category");
+        EventCategories eventCategory = EventCategories.values()[eventCategoryChoice];
+        Event event = new Event();
+    }
+
+    private static void displayAllPastEvents() {
+
+    }
+
+
+    private static void displayAllUpComingEvents() {
+    }
+
+    private static void displayAllEventsWithOpeningSpots() {
+    }
+
+    private static void display5TopEvents() {
+    }
+
+    private static void displayAllEvents() {
+    }
+
+    private static int getUserInput(int maxChoices, String choiceType) {
         Scanner scanner = new Scanner(System.in);
         int choice = -1;
-        while (choice < 0 || choice > 5) {
+        while (choice < 0 || choice > maxChoices) {
             try {
-                System.out.println("Please select your service. ");
+                System.out.println("Please select " + choiceType + ". ");
                 choice = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Input can only be number. ");
@@ -36,6 +95,7 @@ public class Menu {
         System.out.println("3: Display events with opening spot");
         System.out.println("4: Display all upcoming events");
         System.out.println("5: Display all past events");
-    }
+        System.out.println("6: Enter new event");
 
+    }
 }
