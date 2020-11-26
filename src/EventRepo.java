a3dd0abe94658543ea09b40c4429f6db750d6665
Jenny Wong <yintingjenny.wong@gmail.com>
2020-11-26 12:43:09 +0100

    Event Repo added query for upcoming event

--- src/EventRepo.java
+++ src/EventRepo.java
@@ -1,39 +1,50 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class EventRepo {
 
     ArrayList<Event> listOfEvent;
 
-    public EventRepo(){
-
-        listOfEvent=new ArrayList<>();
+    public EventRepo() {
+        listOfEvent = new ArrayList<>();
     }
 
-    ArrayList<Event> getAll(){
-        return listOfEvent;
-    }
-    ArrayList<Event> getBookedOutEvents(){
+    ArrayList<Event> getAll() {
         return listOfEvent;
     }
 
-    public void saveEvent(Event event){
+    ArrayList<Event> getBookedOutEvents() {
+        return listOfEvent;
+    }
+
+    public void saveEvent(Event event) {
         listOfEvent.add(event);
     }
 
-    ArrayList<Event> getFiveTopRatedEvents(){
+    ArrayList<Event> getFiveTopRatedEvents() {
         return listOfEvent;
     }
 
 
-    ArrayList<Event> getPastEvents(){
+    ArrayList<Event> getPastEvents() {
+        Date now = new Date();
+
+        ArrayList<Event> pastEvents = new ArrayList<>();
+        // Put events into the new list if they are in the past
+        for (Event event : getAll()) {
+            if (event.getEventDate().compareTo(now) < 0) {
+                pastEvents.add(event);
+            }
+        }
+
+        return pastEvents;
+    }
+
+    ArrayList<Event> getUpcomingEvents() {
         return listOfEvent;
     }
 
-    ArrayList<Event> getUpcomingEvents(){
-        return listOfEvent;
-    }
-
-    ArrayList<Event> getEventsWithOpenSpots(){
+    ArrayList<Event> getEventsWithOpenSpots() {
         return listOfEvent;
     }
 
